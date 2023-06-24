cee94c2582e5cda6acbd0310cdae84a6fbeab3a6
connor <connor.sw.personal@gmail.com>
2023-06-24 03:50:44 +0000

    Initial Commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, time, datetime
 import yahoo_fin.stock_info as si
 import pandas as pd
 from toolbox import database
@@ -11,16 +11,19 @@ ticker_retreival.set_storage_path(database_path)
 
 
 def main():
-
-    # get all ticker info
-    ticker_info = ticker_retreival.get_all_ticker_information()
-    print(ticker_info)
-
-
-
-
+    last_updated = 0
+    while True:
+        now = time.time()
+        # If it is a new day, update the database
+        if now - last_updated > 86400:
+            print("New Day, updating database")
+            ticker_info = ticker_retreival.get_all_ticker_information()
+            last_updated = now
+            print(ticker_info)
+        time.sleep(10)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
+
